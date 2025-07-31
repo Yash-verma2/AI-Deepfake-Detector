e86fecbe77a7b590619cde515fcf2ed5b6f6c21a
yash verma <yash04523@gmail.com>
2025-07-31 18:09:54 +0530

    debug

--- app.py
+++ app.py
@@ -53,5 +53,6 @@ def index():
 
     return render_template("index.html", result=None)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5002)
+
